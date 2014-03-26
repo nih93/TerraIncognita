82fa28e36e791d9d04f1225033a5950cca909360
nih93 <nihala93@gmail.com>
2014-03-25 22:59:51 -0400

    Update ExtractLargestImage.py

--- ExtractLargestImage.py
+++ ExtractLargestImage.py
@@ -4,7 +4,7 @@ def extract_image(url):
 	print Goose().extract(url).top_image.src
 
 
-print extract_image('http://www.libyaherald.com/2014/03/23/ahly-benghazi-a-new-caf-champion-in-the-making/#axzz2wwOJBuRT')
+
 
 
 
